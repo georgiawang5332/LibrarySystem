1d6d3e62deaad7598836545c10955f0c36d0ec8d
tiroLinda963 <tiroLinda963@outlook.com>
2018-10-01 13:34:45 +0800

    commit first

---
+++ src/library/newBook/Book.java
@@ -0,0 +1,24 @@
+package library.newBook;
+
+
+/**
+ * @since  2018/09/28
+ * @author GG
+ * 優先印出圖書資訊
+ */
+
+public class Book {
+
+	public Book(String string, String string2, String string3, int i, int j) {
+		// TODO 自動產生的建構子 Stub
+	}
+	public Book() {
+		// TODO 自動產生的建構子 Stub
+	}
+	public Object numberB;
+	public Object chineseB;
+	public String engB;
+	public int onShelf;
+	public Object isbn;
+	
+}
---
+++ src/library/newBook/Main.java
@@ -0,0 +1,18 @@
+package library.newBook;
+/**
+ * @since  2018/09/28
+ * @author GG
+ * 優先印出圖書資訊
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		SystemLibrary sysbooks = new SystemLibrary(); 
+		
+		sysbooks.PrintMenu();
+		sysbooks.systemlib();//private
+		
+	}
+
+}
