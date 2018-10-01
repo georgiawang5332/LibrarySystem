9c42885c6359f543664f369c53ed6055fe9efaae
tiroLinda963 <tiroLinda963@outlook.com>
2018-10-01 21:43:13 +0800

    format code

--- src/library/newBook/Book.java
+++ src/library/newBook/Book.java
@@ -1,10 +1,8 @@
 package library.newBook;
 
-
 /**
- * @since  2018/09/28
- * @author GG
- * 優先印出圖書資訊
+ * @since 2018/09/28
+ * @author GG 優先印出圖書資訊
  */
 
 public class Book {
@@ -12,13 +10,15 @@ public class Book {
 	public Book(String string, String string2, String string3, int i, int j) {
 		// TODO 自動產生的建構子 Stub
 	}
+
 	public Book() {
 		// TODO 自動產生的建構子 Stub
 	}
+
 	public Object numberB;
 	public Object chineseB;
 	public String engB;
 	public int onShelf;
 	public Object isbn;
-	
+
 }
--- src/library/newBook/Main.java
+++ src/library/newBook/Main.java
@@ -1,18 +1,16 @@
 package library.newBook;
+
 /**
- * @since  2018/09/28
- * @author GG
- * 優先印出圖書資訊
+ * @since 2018/09/28
+ * @author GG 優先印出圖書資訊
  */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
-		SystemLibrary sysbooks = new SystemLibrary(); 
-		
-		sysbooks.PrintMenu();
-		sysbooks.systemlib();//private
-		
-	}
+		SystemLibrary sysbooks = new SystemLibrary();
 
+		sysbooks.PrintMenu();
+		sysbooks.systemlib();// private
+	}
 }
